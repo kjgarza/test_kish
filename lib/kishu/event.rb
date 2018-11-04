c822e00fb236d2bb67142740aa28bf65d75455b4
Kristian Garza <kj.garza@gmail.com>
2018-11-04 17:47:38 +0100

    safe extract

--- lib/kishu/event.rb
+++ lib/kishu/event.rb
@@ -50,17 +50,17 @@ module Kishu
       unique = event.dig("unique").fetch("buckets", nil)
       # puts unique[1].dig('key')
     
-      unique_regular = unique.find {|access_method| access_method.dig('key').match('regular') }
-      unique_machine = unique.find {|access_method| access_method.dig('key').match('machine') }
-      total_regular  = totale.find {|access_method| access_method.dig('key').match('regular') }
-      total_machine  = totale.find {|access_method| access_method.dig('key').match('machine') }
+      unique_regular = unique.find_all {|access_method| access_method.dig('key').match('regular') }
+      unique_machine = unique.find_all {|access_method| access_method.dig('key').match('machine') }
+      total_regular  = totale.find_all {|access_method| access_method.dig('key').match('regular') }
+      total_machine  = totale.find_all {|access_method| access_method.dig('key').match('machine') }
 
       dataset = { 
         doi: event.dig("key","doi"), 
-        unique_counts_regular: unique_regular.nil? ? 0 : unique_regular.dig("doc_count"),
-        unique_counts_machine: unique_machine.nil? ? 0 : unique_machine.dig("doc_count"),
-        total_counts_regular: total_regular.nil? ? 0 : total_regular.dig("doc_count"),
-        total_counts_machine: total_machine.nil? ? 0:  total_machine.dig("doc_count")
+        unique_counts_regular: unique_regular.empty? ? 0 : unique_regular.size,
+        unique_counts_machine: unique_machine.empty? ? 0 : unique_machine.size,
+        total_counts_regular: total_regular.empty? ? 0 : total_regular.dig(0,"doc_count"),
+        total_counts_machine: total_machine.empty? ? 0:  total_machine.dig(0,"doc_count")
       }
 
 
@@ -84,58 +84,59 @@ module Kishu
     
       conn = Faraday.new(:url => API_URL)
       logger = Logger.new(STDOUT)
-      logger.info totale.size
+      logger.info event.fetch("doc_count")
       
       # arr = dois.map do |dataset| 
         logger.info dataset
-        doi = dataset[:doi]
+        doi = dataset.fetch(:doi,nil)
         json = conn.get "/works/#{doi}"
-        # next unless json.success?
+        return {} unless json.success?
         logger.info "Success on getting metadata for #{doi}"
         data = JSON.parse(json.body)
     
         instances =[
           {
-            count: dataset[:total_counts_regular],
+            count: dataset.fetch(:total_counts_regular),
             "access-method": "regular",
             "metric-type": "total-resolutions"
           },
           {
-            count: dataset[:unique_counts_regular],
+            count: dataset.fetch(:unique_counts_regular),
             "access-method": "regular",
             "metric-type": "unique-resolutions"
           },
           {
-            count: dataset[:unique_counts_machine],
+            count: dataset.fetch(:unique_counts_machine),
             "access-method": "machine",
             "metric-type": "unique-resolutions"
           },
           {
-            count: dataset[:total_counts_machine],
+            count: dataset.fetch(:total_counts_machine),
             "access-method": "machine",
             "metric-type": "total-resolutions"
           },
         ]
     
         instances.delete_if {|instance| instance.dig(:count) <= 0}
-    
         attributes = data.dig("data","attributes")
+        resource_type = attributes.fetch("resource-type-id",nil).nil? ? "dataset" : attributes.fetch("resource-type-id","dataset")
+
         instanced = { 
-          "dataset-id" => [{type: "doi", value: attributes["doi"]}],
-          "data-type" => attributes["resource-type-id"],
-          yop: attributes["published"],
-          uri: attributes["identifier"],
-          publisher: attributes["container-title"],
-          "dataset-title": attributes["title"],
+          "dataset-id" => [{type: "doi", value: attributes.fetch("doi",nil)}],
+          "data-type" => resource_type,
+          yop: attributes.fetch("published",nil),
+          uri: attributes.fetch("identifier",nil),
+          publisher: attributes.fetch("container-title",nil),
+          "dataset-title": attributes.fetch("title",nil),
           "publisher-id": [{
             type: "client-id",
-            value: attributes["data-center-id"]
+            value: attributes.fetch("data-center-id",nil)
           }],
           "dataset-dates": [{
             type: "pub-date",
-            value: attributes["published"]
+            value: attributes.fetch("published",nil)
           }],
-          "dataset-contributors": attributes["author"].map { |a| get_authors(a) },
+          "dataset-contributors": attributes.fetch("author",[]).map { |a| get_authors(a) },
           platform: "datacite",
           performance: [{
             period: @period,
@@ -147,6 +148,7 @@ module Kishu
       # arr.map! do |instance|
       #   LogStash::Event.new(instance)
       # end
+     
     
       instanced
     end
@@ -155,10 +157,10 @@ module Kishu
     def get_authors author
       if (author.key?("given") && author.key?("family"))
         { type: "name",
-          value: author["given"]+" "+author["family"] }
+          value: author.fetch("given",nil)+" "+author.fetch("family",nil) }
         elsif author.key?("literal")
           { type: "name",
-            value: author["literal"] }
+            value: author.fetch("literal",nil) }
         else 
           { type: "name",
             value: "" }
--- lib/kishu/report.rb
+++ lib/kishu/report.rb
@@ -1,6 +1,8 @@
 require 'json'
 require 'date'
 require "faraday"
+require 'securerandom'
+
 
 # require_relative 'event'
 
@@ -9,61 +11,124 @@ module Kishu
 
     # include Kishu::Event
 
-    FILENAME = "../../tmp/output.json"
+
+    def report_period options={}
+      es_client = Client.new()
+      logdate = es_client.get_logdate({aggs_size: 1})
+      puts logdate
+      Date.parse(logdate)
+    end
 
 
     def get_events options={}
+      logger = Logger.new(STDOUT)
       es_client = Client.new()
-      aggs = es_client.get({aggs_size: options[:aggs_size] || 500, after_key:""})
-      # puts aggs.class
-      x = aggs.map do |event|
-        wrap_event event
+      response = es_client.get({aggs_size: options[:aggs_size] || 20, after_key: options[:after_key] ||=""})
+      aggs = response.dig("aggregations","doi","buckets")
+      x = aggs.map do |agg|
+        wrap_event agg
       end
-      x = x.reject { |e| e["data-type"] != "dataset" } # returns [1, 3]
-      x
+      puts x.size
+      # a = x.find_all {|e| e.dig("data-type") == 'dataset' }
+      b = x.reject {|e| e.fetch("data-type",nil) == nil }
+      a = b.find_all {|e| e.fetch("data-type","dataset") == 'dataset' }
+      # a = x.reject { |e| e.fetch("data-type","dataset") != "dataset" } # returns [1, 3]
+      # puts a.size
+      after = response.dig("aggregations","doi","after_key","doi")
+      logger.info "After_key for pagination #{after}"
+      y = {data: a, after: after}
+      y
     end
 
-    def get_paginated
-      Cursor.new
+    # def get_paginated
+    #   # Cursor.new
+    # end
+
+    # def get_all_events
+    #   # aggs= get_paginated
+
+    #   # puts aggs.each.lazy
+
+    #   # x = aggs.map do |event|
+    #   #   wrap_event event
+    #   # end
+    #   # x = x.reject { |e| e["data-type"] != "dataset" } # returns [1, 3]
+    #   # x
+    # end
+
+
+    def generate_files
+      # n =0
+
+      loop do
+        response = get_events({after_key: @after ||=""})
+        File.open("tmp/datasets-#{SecureRandom.hex}.json","w") do |f|
+          response[:data].each do |instance|
+            separator = response[:after].empty? ? "" : ","
+            f.write(instance.to_json+separator+"\n")
+          end
+        end
+        # break if n == 3
+        @after = response[:after]
+        break if @after.empty?
+        
+      end
     end
 
-    def get_all_events
-      aggs= get_paginated
-
-      puts aggs.each.lazy
-
-
-      # x = aggs.map do |event|
-      #   wrap_event event
-      # end
-      # x = x.reject { |e| e["data-type"] != "dataset" } # returns [1, 3]
-      # x
+    def merged_file
+      "reports/datacite_resolution_report_#{report_period.strftime("%Y-%m")}.json"
     end
+
+    def merge_report    
+      generate_files
+      system("cat tmp/datasets-* > #{merged_file}")
+      puts "Merged Completed"
+    end
+
+    def make_report_2
+      # logdate= "2018-04-01"
+      report_header = '{"report-header": '+get_header.to_json.to_s+',"report-datasets": [ '+"\n"
+      
+      File.open("tmp/datasets-00-report-header.json","w") do |f|
+        f.write(report_header)
+      end
+      report_footer = ']'+"\n"+'}'
+      
+      File.open("tmp/datasets-zz99-report-footer.json","w") do |f|
+        f.write(report_footer)
+      end
+      merge_report
+    end
+
 
     def make_report
-      logdate= "2018-04-05"
-      report = {
-        "report-header": get_header(logdate),
-        "report-datasets": get_events
-      }
-      File.open("DataCite-access.log-_#{logdate}.json","w") do |f|
-        f.write(report.to_json)
-      end
+      merge_report
+      # logdate= "2018-04-05"
+      # report = {
+      #   "report-header": get_header(logdate),
+      #   "report-datasets": get_events.dig(:data)
+      # }
+      
+      # File.open("datacite-resolution-report-#{logdate}.json","w") do |f|
+      #   f.write(report.to_json)
+      # end
     end
 
-    def set_period year_month
-      # PERIOD = { 
-      #   "begin-date": Date.civil(date.year, date.mon, 1).strftime("%Y-%m-%d"),
-      #   "end-date": Date.civil(date.year, date.mon, -1).strftime("%Y-%m-%d"),
-      # }
-      @period = {"begin-date": "2018-04-01","end-date": "2018-04-30",}
+    def set_period 
+      report_period
+      @period = { 
+        "begin-date": Date.civil(report_period.year, report_period.mon, 1).strftime("%Y-%m-%d"),
+        "end-date": Date.civil(report_period.year, report_period.mon, -1).strftime("%Y-%m-%d"),
+      }
     end
 
     def get_report year_month 
       # logdate= "2018-04-05"
-      set_period year_month
+      set_period 
 
-      make_report
+      # make_report
+      make_report_2 
+      clean_tmp 
       # send_report get_header(logdate).dig("report-id"), logdate
     end
 
@@ -80,23 +145,37 @@ module Kishu
 
     end
 
-    def get_header logdate
-    date = Date.parse(logdate)
-    {
-      "report-name": "resolution report",
-        "report-id": "dsr",
-        "release": "rd1",
-        "created": Date.today.strftime("%Y-%m-%d"),
-        "created-by": "datacite",
-        "reporting-period": 
-        {
-            "begin-date": Date.civil(date.year, date.mon, 1).strftime("%Y-%m-%d"),
-            "end-date": Date.civil(date.year, date.mon, -1).strftime("%Y-%m-%d")
-        },
-        "report-filters": [ ],
-        "report-attributes": [ ],
-        "exceptions": [ ]
-      }
+    # def get_header logdate
+    # date = Date.parse(logdate)
+    # {
+    #   "report-name": "resolution report",
+    #     "report-id": "dsr",
+    #     "release": "rd1",
+    #     "created": Date.today.strftime("%Y-%m-%d"),
+    #     "created-by": "datacite",
+    #     "reporting-period": 
+    #     {
+    #         "begin-date": Date.civil(date.year, date.mon, 1).strftime("%Y-%m-%d"),
+    #         "end-date": Date.civil(date.year, date.mon, -1).strftime("%Y-%m-%d")
+    #     },
+    #     "report-filters": [ ],
+    #     "report-attributes": [ ],
+    #     "exceptions": [ ]
+    #   }
+    # end
+
+    def get_header 
+      {
+        "report-name": "resolution report",
+          "report-id": "dsr",
+          "release": "rd1",
+          "created": Date.today.strftime("%Y-%m-%d"),
+          "created-by": "datacite",
+          "reporting-period": @period,
+          "report-filters": [ ],
+          "report-attributes": [ ],
+          "exceptions": [ ]
+        }
     end
   end
 end
