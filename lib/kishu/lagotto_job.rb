95aa96eefe4eebdabe1e9c43078fa4aa30fb8a84
Kristian Garza <kj.garza@gmail.com>
2018-11-26 16:29:42 +0100

    job

--- lib/kishu/lagotto_job.rb
+++ lib/kishu/lagotto_job.rb
@@ -1,10 +1,22 @@
+require_relative 'resolution_event'
+
+
 class LagottoJob
   include SuckerPunch::Job
+  include Kishu::Utils
   workers 4 
-  max_jobs 10
 
 
-  def perform(event)
-    ResolutionEvent.push_instance(event).track
+
+  def perform(event, options={})
+    data = format_instance event, options
+    ENV['LAGOTTINO_URL'] = "https://api.test.datacite.org"
+
+    push_url = ENV['LAGOTTINO_URL']  + "/events"
+    response =  Maremma.post(push_url, data: data.to_json,
+                  bearer: token,
+                  content_type: 'application/vnd.api+json')
+    puts data
+    puts response.status
   end
 end
