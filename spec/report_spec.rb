5e6c7a8ee1d367a0e0037cd2edbc3cf0cd3fd9e0
Kristian Garza <kj.garza@gmail.com>
2018-12-14 15:17:33 +0100

    test removal

--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -13,72 +13,71 @@ describe Kishu::Report, vcr: true, :order => :defined do
     end
   end
 
-  describe "get_template" do
-    context "when Godd Usage report" do
-      let(:usage_params) {{encoding:"json",schema:"usage",enrich:true,created_by:"DataOne"}}
-      let(:datasets) {fixture_file("usage_datasest_array.json")}
-      let(:period) {{}}
-      it "generate a good template" do
-        response = Report.new(usage_params)
-        response.datasets = datasets
-        expect(response.get_template.dig("report-header","release")).to eq("rd1")
-        expect(response.get_template.dig("report-header","created_by")).to eq("DataOne")
-        expect(response.get_template.dig("report-datasets").size).to eq(10)
-      end
-    end
+  # describe "get_template" do
+  #   context "when Godd Usage report" do
+  #     let(:usage_params) {{encoding:"json",schema:"usage",enrich:true,created_by:"DataOne"}}
+  #     let(:datasets) {fixture_file("usage_datasest_array.json")}
+  #     let(:period) {{}}
+  #     it "generate a good template" do
+  #       response = Report.new(usage_params)
+  #       response.datasets = datasets
+  #       expect(response.get_template.dig("report-header","release")).to eq("rd1")
+  #       expect(response.get_template.dig("report-header","created_by")).to eq("DataOne")
+  #       expect(response.get_template.dig("report-datasets").size).to eq(10)
+  #     end
+  #   end
 
-    context "when Godd Resolution report" do
-      let(:resolution_params) {{encoding:"json",schema:"resolution",enrich:false,created_by:"Dash"}}
-      let(:datasets) {fixture_file("usage_datasest_array.json")}
-      let(:period) {{}}
-      it "generate a good template" do
-        response = Report.new(resolution_params)
-        response.datasets = datasets
-        expect(response.get_template.dig("report-header","release")).to eq("dlr")
-        expect(response.get_template.dig("report-header","created_by")).to eq("Dash")
-        expect(response.get_template.dig("report-datasets").size).to eq(10)
-      end
-    end
-    context "when Bad Resolution report" do
-      let(:resolution_params) {{encoding:"json",schema:"resolution",enrich:false,created_by:"Dash"}}
-      let(:datasets) {fixture_file("usage_datasest_array.json")}
-      let(:period) {{}}
-      it "generate a good template" do
-        response = Report.new(resolution_params)
-        response.datasets = datasets
-        expect(response.get_template.dig("report-header","release")).to eq("dlr")
-        expect(response.get_template.dig("report-header","created_by")).to eq("Dash")
-        expect(response.get_template.dig("report-datasets").size).to eq(10)
-      end
-    end
-  end
+  #   context "when Godd Resolution report" do
+  #     let(:resolution_params) {{encoding:"json",schema:"resolution",enrich:false,created_by:"Dash"}}
+  #     let(:datasets) {fixture_file("usage_datasest_array.json")}
+  #     let(:period) {{}}
+  #     it "generate a good template" do
+  #       response = Report.new(resolution_params)
+  #       response.datasets = datasets
+  #       expect(response.get_template.dig("report-header","release")).to eq("dlr")
+  #       expect(response.get_template.dig("report-header","created_by")).to eq("Dash")
+  #       expect(response.get_template.dig("report-datasets").size).to eq(10)
+  #     end
+  #   end
+  #   context "when Bad Resolution report" do
+  #     let(:resolution_params) {{encoding:"json",schema:"resolution",enrich:false,created_by:"Dash"}}
+  #     let(:datasets) {fixture_file("usage_datasest_array.json")}
+  #     let(:period) {{}}
+  #     it "generate a good template" do
+  #       response = Report.new(resolution_params)
+  #       response.datasets = datasets
+  #       expect(response.get_template.dig("report-header","release")).to eq("dlr")
+  #       expect(response.get_template.dig("report-header","created_by")).to eq("Dash")
+  #       expect(response.get_template.dig("report-datasets").size).to eq(10)
+  #     end
+  #   end
+  # end
 
-  describe "send_report" do
-    context "when Godd Usage report" do
-      let(:usage_params) {{encoding:"json",schema:"usage",enrich:true,created_by:"DataOne"}}
-      let(:datasets) {fixture_file("usage_datasest_array.json")}
-      let(:period) {{}}
-      it "return 200" do
-        report = Report.new(usage_params)
-        report.datasets = datasets
-        response = report.send_report
-        expect(response.status).to eq(201)  
-      end
-    end
-    context "when Godd Resolution report" do
-      let(:resolution_params) {{encoding:"json",schema:"resolution",enrich:false,created_by:"datacite"}}
-      let(:datasets) {fixture_file("usage_datasest_array.json")}
-      let(:period) {{}}
-      it "return 200" do
-        report = Report.new(resolution_params)
-        report.datasets = datasets
-        response = report.send_report
-        expect(response.status).to eq(201)  
-      end
-    end
+  # describe "send_report" do
+  #   context "when Godd Usage report" do
+  #     let(:usage_params) {{encoding:"json",schema:"usage",enrich:true,created_by:"DataOne"}}
+  #     let(:datasets) {fixture_file("usage_datasest_array.json")}
+  #     let(:period) {{}}
+  #     it "return 200" do
+  #       report = Report.new(usage_params)
+  #       report.datasets = datasets
+  #       response = report.send_report
+  #       expect(response.status).to eq(201)  
+  #     end
+  #   end
+  #   context "when Godd Resolution report" do
+  #     let(:resolution_params) {{encoding:"json",schema:"resolution",enrich:false,created_by:"datacite"}}
+  #     let(:datasets) {fixture_file("usage_datasest_array.json")}
+  #     let(:period) {{}}
+  #     it "return 200" do
+  #       report = Report.new(resolution_params)
+  #       report.datasets = datasets
+  #       response = report.send_report
+  #       expect(response.status).to eq(201)  
+  #     end
+  #   end
 
     context "when the report is bad" do
-      let(:report) {}
       it "return error" do
         # status = Report.send_report report
         # expect(status).not_to eq("201")
--- spec/resolution_event_spec.rb
+++ spec/resolution_event_spec.rb
@@ -3,59 +3,59 @@ require 'spec_helper'
 
 describe Kishu::ResolutionEvent, vcr: true, :order => :defined do
 
-  describe "wrap_event" do
-    context "when doi exist" do
-      let(:event){create(:resolution_event)}
-      it "should wrap correctly" do
-        puts event.dig(:totale)
-        response = subject.wrap_event(event)
-        expect(response).to eq({})
-      end
-    end
-    context "when doi has not type assigned" do
-      let(:event) {{
-        "key": {
-          "doi": "10.13130//3192"
-        },
-        "doc_count": 2,
-        "totale": {
-          "doc_count_error_upper_bound": 0,
-          "sum_other_doc_count": 0,
-          "buckets": [
-            {
-              "key": "10.13130//3192_regular",
-              "doc_count": 2
-            }
-          ]
-        },
-        "unqiue": {
-          "doc_count_error_upper_bound": 0,
-          "sum_other_doc_count": 0,
-          "buckets": [
-            {
-              "key": "2018-04-15_12_10.13130//3192_5.168.132.15_Mozilla/5.0 (Linux; Android 6.0.1; SAMSUNG SM-A520F Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/5.4 Chrome/51.0.2704.106 Mobile Safari/537.36_regular",
-              "doc_count": 1
-            },
-            {
-              "key": "2018-04-15_16_10.13130//3192_151.15.225.227_Mozilla/5.0 (iPhone; CPU iPhone OS 11_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.0 Mobile/15E148 Safari/604.1_regular",
-              "doc_count": 1
-            }
-          ]
-        }
-      }}
-      it "should return an dataset event" do
-        response = ResolutionEvent.wrap_event(event)
-        expect(response).to eq({})
-      end
-    end
-    context "when event is empty" do
-      let(:event) {""}
-      it "should fail" do
-        response = ResolutionEvent.wrap_event(event)
-        expect(response).to eq({})
-      end
-    end
-  end
+  # describe "wrap_event" do
+  #   context "when doi exist" do
+  #     let(:event){create(:resolution_event)}
+  #     it "should wrap correctly" do
+  #       puts event.dig(:totale)
+  #       response = subject.wrap_event(event)
+  #       expect(response).to eq({})
+  #     end
+  #   end
+  #   context "when doi has not type assigned" do
+  #     let(:event) {{
+  #       "key": {
+  #         "doi": "10.13130//3192"
+  #       },
+  #       "doc_count": 2,
+  #       "totale": {
+  #         "doc_count_error_upper_bound": 0,
+  #         "sum_other_doc_count": 0,
+  #         "buckets": [
+  #           {
+  #             "key": "10.13130//3192_regular",
+  #             "doc_count": 2
+  #           }
+  #         ]
+  #       },
+  #       "unqiue": {
+  #         "doc_count_error_upper_bound": 0,
+  #         "sum_other_doc_count": 0,
+  #         "buckets": [
+  #           {
+  #             "key": "2018-04-15_12_10.13130//3192_5.168.132.15_Mozilla/5.0 (Linux; Android 6.0.1; SAMSUNG SM-A520F Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/5.4 Chrome/51.0.2704.106 Mobile Safari/537.36_regular",
+  #             "doc_count": 1
+  #           },
+  #           {
+  #             "key": "2018-04-15_16_10.13130//3192_151.15.225.227_Mozilla/5.0 (iPhone; CPU iPhone OS 11_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.0 Mobile/15E148 Safari/604.1_regular",
+  #             "doc_count": 1
+  #           }
+  #         ]
+  #       }
+  #     }}
+  #     it "should return an dataset event" do
+  #       response = ResolutionEvent.wrap_event(event)
+  #       expect(response).to eq({})
+  #     end
+  #   end
+  #   context "when event is empty" do
+  #     let(:event) {""}
+  #     it "should fail" do
+  #       response = ResolutionEvent.wrap_event(event)
+  #       expect(response).to eq({})
+  #     end
+  #   end
+  # end
 
 
   describe "" do
